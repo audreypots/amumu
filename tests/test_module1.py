863ec77cf8666324010e3a177ed31155c1b49a0b
Maurice Gonzales <mau.infrance@gmail.com>
2018-08-22 19:33:37 +0800

    removed sys.path.append

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -3,11 +3,6 @@ Test module for module1.py
 """
 import sys
 import unittest
-
-#TODO: working in Windows even if the path is wrong in sys.path.append,
-# module was still imported if discover is used and not running the test module.py
-sys.path.append("/Users/mauricegonzales/Documents/PYTHON/iso_dual_2.19_parser")
-
 from main.modules import module1
 
 class Module1Test(unittest.TestCase):
