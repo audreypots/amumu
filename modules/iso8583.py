25e629077246d45e53b37915b9e1576ab2c1fc69
Maurice <mau.infrance@gmail.com>
2018-08-31 17:08:40 +0800

    Added a bunch of fields, 31 and 35 not yet done

--- modules/iso8583.py
+++ modules/iso8583.py
@@ -23,7 +23,7 @@ class Iso8583(object):
             is_raw_packet_empty: check if raw_packet is empty
                 return: True or False
             pop_value_in_packet: pop value in raw_packet
-            display: display unpacked packet 
+            display: display unpacked packet
                 return: formated fields
 
             FIELD METHODS: Loads the Field and returns valid or invalid
@@ -36,7 +36,15 @@ class Iso8583(object):
             load_field13: Local Transaction Date N 4
             load_field14: Expiration Date N 4
             load_field22: Point of Service Entry Mode N 3 (4)
-            load_field23: EMV Card Sequence Number N 3
+            load_field23: EMV Card Sequence Number N 3 (4)
+            load_field24: Network International Identifier N3 (4)
+            load_field25: Point of Service Condition Code N 2
+            load_field27: Additional POS information N 2
+            load_field31: Acquirer Reference Data AN..1
+            load_field35: Track II Data Z..37
+            load_field37: Reference Number ANS 12
+            load_field38: Approval Code AN 6
+            load_field39: Response Code AN 2
 
             load_field63: Private Use
     """
@@ -50,7 +58,10 @@ class Iso8583(object):
     DATE_SIZE = 4
     EXP_DATE_SIZE = 4
     POSSEM_SIZE = 4 #3
-    EMV_CARD_SEQ_SIZE = 4 #3 TODO
+    EMV_CARD_SEQ_SIZE = 4 #3
+    NII_SIZE = 4 #3
+    POS_CONDITION_CODE_SIZE = 2
+    ADD_POS_INFORMATION_SIZE = 2
 
     def __init__(self):
         """Return an empty ISO858 object"""
@@ -70,6 +81,14 @@ class Iso8583(object):
             14: self.load_field14,
             22: self.load_field22,
             23: self.load_field23,
+            24: self.load_field24,
+            25: self.load_field25,
+            27: self.load_field27,
+            31: self.load_field31,
+            35: self.load_field35,
+            37: self.load_field37,
+            38: self.load_field38,
+            39: self.load_field39,
             63: self.load_field63
             })
         self.error_msg = ""
@@ -118,7 +137,6 @@ class Iso8583(object):
             bit = 8
             for y_counter in range(0, 4):
                 if int(self.bitmap[x_counter], 16) & bit:
-                    #self.fields[field+y_counter] = ""
                     if self.load_field[field+y_counter]() != "valid":
                         return "invalid"
                 bit /= 2
@@ -144,7 +162,7 @@ class Iso8583(object):
         return popped
 
     def display(self):
-        #TODO no unit test
+        """display formatted packet"""
         output_text = "  : " + self.tpdu + "\n  : "
         output_text += self.msg_type + "\n  : "
         output_text += self.bitmap + "\n"
@@ -222,8 +240,7 @@ class Iso8583(object):
             self.set_errmsg(err_msg)
             return "invalid"
         #get STAN
-        stan = self.pop_value_in_packet(
-            self.STAN_SIZE)
+        stan = self.pop_value_in_packet(self.STAN_SIZE)
         #check if n 3
         if len(stan) != self.STAN_SIZE:
             self.set_errmsg(err_msg)
@@ -233,32 +250,183 @@ class Iso8583(object):
 
     def load_field12(self):
         """ field 12 n 6 transaction time"""
-        #TODO not really needed
-        self.fields[12] = ""
-        return "valid"
+        result_value = "valid"
+        err_msg = "field12"
+        #check first if raw_packet has value
+        if self.is_raw_packet_empty():
+            self.set_errmsg(err_msg)
+            return "invalid"
+        #get Time
+        time = self.pop_value_in_packet(self.TIME_SIZE)
+        #check if n 6
+        if len(time) != self.TIME_SIZE:
+            self.set_errmsg(err_msg)
+            return "invalid"
+        self.fields[12] = time
+        return result_value
 
     def load_field13(self):
         """ field 13 n 4 transaction date"""
-        #TODO not really needed
-        self.fields[13] = ""
-        return "valid"
+        result_value = "valid"
+        err_msg = "field13"
+        #check first if raw_packet has value
+        if self.is_raw_packet_empty():
+            self.set_errmsg(err_msg)
+            return "invalid"
+        #get date
+        date = self.pop_value_in_packet(self.DATE_SIZE)
+        #check if n 4
+        if len(date) != self.DATE_SIZE:
+            self.set_errmsg(err_msg)
+            return "invalid"
+        self.fields[13] = date
+        return result_value
 
     def load_field14(self):
         """ field 14 n 4 expiry date"""
-        #TODO not really needed
-        self.fields[14] = ""
-        return "valid"
+        result_value = "valid"
+        err_msg = "field14"
+        #check first if raw_packet has value
+        if self.is_raw_packet_empty():
+            self.set_errmsg(err_msg)
+            return "invalid"
+        #get exp date
+        exp_date = self.pop_value_in_packet(self.EXP_DATE_SIZE)
+        #check if n 4
+        if len(exp_date) != self.EXP_DATE_SIZE:
+            self.set_errmsg(err_msg)
+            return "invalid"
+        self.fields[14] = exp_date
+        return result_value
 
     def load_field22(self):
-        """ field 22 n 3 possem (really n 4)"""
-        #TODO not really needed
-        self.fields[22] = ""
-        return "valid"
+        """ field 22 n 4 possem (3 in specs)"""
+        result_value = "valid"
+        err_msg = "field22"
+        #check first if raw_packet has value
+        if self.is_raw_packet_empty():
+            self.set_errmsg(err_msg)
+            return "invalid"
+        #get possem
+        possem = self.pop_value_in_packet(self.POSSEM_SIZE)
+        #check if n 4 (3 in specs)
+        if len(possem) != self.POSSEM_SIZE:
+            self.set_errmsg(err_msg)
+            return "invalid"
+        self.fields[22] = possem
+        return result_value
 
     def load_field23(self):
         """ field 23 n 3 EMV card sequence number"""
+        result_value = "valid"
+        err_msg = "field23"
+        #check first if raw_packet has value
+        if self.is_raw_packet_empty():
+            self.set_errmsg(err_msg)
+            return "invalid"
+        #get emv_card_seq
+        emv_card_seq = self.pop_value_in_packet(self.EMV_CARD_SEQ_SIZE)
+        #check if n 4 (3 in specs)
+        if len(emv_card_seq) != self.POSSEM_SIZE:
+            self.set_errmsg(err_msg)
+            return "invalid"
+        self.fields[23] = emv_card_seq
+        return result_value
+
+    def load_field24(self):
+        """load field 24 n 4 (3 in specs) 
+        network international identifier
+        """
+        result_value = "valid"
+        err_msg = "field24"
+        #check first if raw_packet has value
+        if self.is_raw_packet_empty():
+            self.set_errmsg(err_msg)
+            return "invalid"
+        #get nii
+        nii = self.pop_value_in_packet(self.NII_SIZE)
+        #check if n 4 (3 in specs)
+        if len(nii) != self.NII_SIZE:
+            self.set_errmsg(err_msg)
+            return "invalid"
+        self.fields[24] = nii
+        return result_value
+
+    def load_field25(self):
+        """load field 25 n 2 point of service condition code"""
+        result_value = "valid"
+        err_msg = "field25"
+        #check first if raw_packet has value
+        if self.is_raw_packet_empty():
+            self.set_errmsg(err_msg)
+            return "invalid"
+        #get pos condition code
+        pos_condition_code = self.pop_value_in_packet(self.POS_CONDITION_CODE_SIZE)
+        #check if n 2
+        if len(pos_condition_code) != self.POS_CONDITION_CODE_SIZE:
+            self.set_errmsg(err_msg)
+            return "invalid"
+        self.fields[25] = pos_condition_code
+        return result_value
+
+    def load_field27(self):
+        """load field 27 n 2 additional pos information"""
+        result_value = "valid"
+        err_msg = "field27"
+        #check first if raw_packet has value
+        if self.is_raw_packet_empty():
+            self.set_errmsg(err_msg)
+            return "invalid"
+        #get add pos information
+        add_pos_information = self.pop_value_in_packet(self.ADD_POS_INFORMATION_SIZE)
+        #check if n 2
+        if len(add_pos_information) != self.ADD_POS_INFORMATION_SIZE:
+            self.set_errmsg(err_msg)
+            return "invalid"
+        self.fields[27] = add_pos_information
+        return result_value
+
+    def load_field31(self):
+        """load field 31 an..1 acquirer reference data"""
+        result_value = "valid"
+        err_msg = "field31"
+        #check first if raw_packet has value
+        if self.is_raw_packet_empty():
+            self.set_errmsg(err_msg)
+            return "invalid"
+        #get size first
+        size = int(self_pop)
+        #get add pos information
+        acq_ref_data = self.pop_value_in_packet(self.ADD_POS_INFORMATION_SIZE)
+        #check if n 2
+        if len(add_pos_information) != self.ADD_POS_INFORMATION_SIZE:
+            self.set_errmsg(err_msg)
+            return "invalid"
+        self.fields[27] = add_pos_information
+        return result_value
+
+    def load_field35(self):
+        """load field 35 z..37 track II data"""
         #TODO not really needed
-        self.fields[23] = ""
+        self.fields[35] = ""
+        return "valid"
+
+    def load_field37(self):
+        """load field 37 ans 12 reference number"""
+        #TODO not really needed
+        self.fields[37] = ""
+        return "valid"
+
+    def load_field38(self):
+        """load field 38 an 6 approval code"""
+        #TODO not really needed
+        self.fields[38] = ""
+        return "valid"
+
+    def load_field39(self):
+        """load field 39 an 2 response code"""
+        #TODO not really needed
+        self.fields[39] = ""
         return "valid"
 
     def load_field63(self):
--- tests/test_iso8583.py
+++ tests/test_iso8583.py
@@ -154,12 +154,174 @@ class ISO8583Test(unittest.TestCase):
         test for field 12
         """
         self.assertEqual(self.iso.unpack(
-            "123456789002000020000000000000123456"), "valid")
-        self.assertEquals(self.iso.fields[11], "123456")
-        #negative: n not equal to 6. Field 11 only
+            "123456789002000010000000000000654321"), "valid")
+        self.assertEquals(self.iso.fields[12], "654321")
+        #negative: n not equal to 6. Field 12 only
         self.assertEqual(self.iso.unpack(
-            "1234567890020000200000000000001234"), "invalid")
-        self.assertEqual(self.iso.error_msg, "field11")
+            "1234567890020000100000000000001234"), "invalid")
+        self.assertEqual(self.iso.error_msg, "field12")
+
+    def test_iso_field_13_values_(self):
+        """
+        test for field 13
+        """
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0008000000000000" #bitmap
+        packet += "0831" #field 13
+        self.assertEqual(self.iso.unpack(packet), "valid")
+        self.assertEquals(self.iso.fields[13], "0831")
+        #negative: n not equal to 4. Field 13 only
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0008000000000000" #bitmap
+        packet += "083" #field 13
+        self.assertEqual(self.iso.unpack(packet), "invalid")
+        self.assertEqual(self.iso.error_msg, "field13")
+
+    def test_iso_field_14_values_(self):
+        """
+        test for field 14
+        """
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0004000000000000" #bitmap
+        packet += "0820" #field 14
+        self.assertEqual(self.iso.unpack(packet), "valid")
+        self.assertEquals(self.iso.fields[14], "0820")
+        #negative: n not equal to 4. Field 14 only
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0004000000000000" #bitmap
+        packet += "08" #field 13
+        self.assertEqual(self.iso.unpack(packet), "invalid")
+        self.assertEqual(self.iso.error_msg, "field14")
+
+    def test_iso_field_22_values_(self):
+        """
+        test for field 22
+        """
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000040000000000" #bitmap
+        packet += "0051" #field 22
+        self.assertEqual(self.iso.unpack(packet), "valid")
+        self.assertEquals(self.iso.fields[22], "0051")
+        #negative: n not equal to 4 (3 in specs). Field 22 only
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000040000000000" #bitmap
+        packet += "00" #field 22
+        self.assertEqual(self.iso.unpack(packet), "invalid")
+        self.assertEqual(self.iso.error_msg, "field22")
+
+    def test_iso_field_23_values_(self):
+        """
+        test for field 23
+        """
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000020000000000" #bitmap
+        packet += "0001" #field 23
+        self.assertEqual(self.iso.unpack(packet), "valid")
+        self.assertEquals(self.iso.fields[23], "0001")
+        #negative: n not equal to 4 (3 in specs). Field 23 only
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000020000000000" #bitmap
+        packet += "001" #field 23
+        self.assertEqual(self.iso.unpack(packet), "invalid")
+        self.assertEqual(self.iso.error_msg, "field23")
+
+    def test_iso_field_24_values_(self):
+        """
+        test for field 24
+        """
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000010000000000" #bitmap
+        packet += "0048" #field 24
+        self.assertEqual(self.iso.unpack(packet), "valid")
+        self.assertEquals(self.iso.fields[24], "0048")
+        #negative: n not equal to 3 (3 in specs). Field 24 only
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000010000000000" #bitmap
+        packet += "001" #field 24
+        self.assertEqual(self.iso.unpack(packet), "invalid")
+        self.assertEqual(self.iso.error_msg, "field24")
+
+    def test_iso_field_25_values_(self):
+        """
+        test for field 25
+        """
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000008000000000" #bitmap
+        packet += "00" #field 25
+        self.assertEqual(self.iso.unpack(packet), "valid")
+        self.assertEquals(self.iso.fields[25], "00")
+        #negative: n not equal to 2. Field 25 only
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000008000000000" #bitmap
+        packet += "0" #field 25
+        self.assertEqual(self.iso.unpack(packet), "invalid")
+        self.assertEqual(self.iso.error_msg, "field25")
+
+    def test_iso_field_27_values_(self):
+        """
+        test for field 27
+        """
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000002000000000" #bitmap
+        packet += "00" #field 27
+        self.assertEqual(self.iso.unpack(packet), "valid")
+        self.assertEquals(self.iso.fields[27], "00")
+        #negative: n not equal to 2. Field 27 only
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000002000000000" #bitmap
+        packet += "0" #field 27
+        self.assertEqual(self.iso.unpack(packet), "invalid")
+        self.assertEqual(self.iso.error_msg, "field27")
+
+    def test_iso_field_31_values_(self):
+        """
+        test for field 31
+        """
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000000200000000" #bitmap
+        packet += "0131" #field 31
+        self.assertEqual(self.iso.unpack(packet), "valid")
+        self.assertEquals(self.iso.fields[31], "0131")
+        #negative: .. not 4
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000000200000000" #bitmap
+        packet += "0" #field 31
+        self.assertEqual(self.iso.unpack(packet), "invalid")
+        self.assertEqual(self.iso.error_msg, "field31")
+
+    def test_iso_field_35_values_(self):
+        """
+        test for field 35
+        """
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000000020000000" #bitmap
+        packet += "295435560000000007D120810123456F" #field 35 #TODO
+        self.assertEqual(self.iso.unpack(packet), "valid")
+        self.assertEquals(self.iso.fields[35], "5435560000000007D120810123456F")
+        #negative: not z ..37
+        packet = "1234567890" #tpdu
+        packet += "0200" #msg_type
+        packet += "0000000020000000" #bitmap
+        packet += "295435560000000007D12081012" #field 35
+        self.assertEqual(self.iso.unpack(packet), "invalid")
+        self.assertEqual(self.iso.error_msg, "field35")
 
     def test_iso_unpack_input_chk(self):
         """
